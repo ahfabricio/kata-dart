1b1d64196da9524cec060d402c67efea955602d3
Fabricio Alegria <f.alegria.herrera@octo.com>
2022-06-03 16:23:22 +0200

    bootstrap APi with supa

---
+++ bin/kata_dart.dart
@@ -0,0 +1,5 @@
+import 'package:kata_dart/kata_dart.dart' as kata_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kata_dart.calculate()}!');
+}
---
+++ test/kata_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:kata_dart/kata_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
